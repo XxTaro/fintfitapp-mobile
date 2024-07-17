f26328c5d83ce8fb7b808cea33b0cc739f88f024
Gustavo Lopes <pstarofoli@gmail.com>
2024-07-17 20:38:50 -0300

    ### Added  - date line in transaction page  - scroll view in transaction page (still bugged)

--- lib/transaction_page.dart
+++ lib/transaction_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class TransactionPage extends StatefulWidget {
   const TransactionPage({super.key});
@@ -9,9 +11,25 @@ class TransactionPage extends StatefulWidget {
 
 class _TransactionPage extends State<TransactionPage> {
 
+  List<String> items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10'];
+
+  late List<Widget> containers = [];
+
+
   @override
   void initState() {
     super.initState();
+    containers = items.map((item) {
+      return Container(
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
+        color: Colors.blue,
+        child: Text(
+          item,
+          style: TextStyle(color: Colors.white),
+        ),
+      );
+    }).toList();
   }
 
   @override
@@ -20,14 +38,15 @@ class _TransactionPage extends State<TransactionPage> {
       child: Column(
         children: [
           _buildTransactionHeader(),
-          Center(child: Row(
-            children: [
-              Icon(Icons.arrow_back_ios_new_rounded),
-              Text('data'),
-              Icon(Icons.arrow_forward_ios_rounded)
-            ],
-          ))
-          
+          _buildDateSelection(),
+          Stack(
+            children: [SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Column(
+              children: containers
+            ),
+          )]
+          )
         ],
       )
     );
@@ -45,6 +64,34 @@ class _TransactionPage extends State<TransactionPage> {
           Positioned(
             right: 0, 
             child: Icon(Icons.filter_alt_off, size: 28)
+          )
+        ],
+      )
+    );
+  }
+
+  Widget _buildDateSelection() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30), 
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          const Center(
+            child: Text('Data', style: TextStyle(fontSize: 18))
+          ),
+          Positioned(
+            left: 0,
+            child: Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: const Icon(Icons.arrow_back_ios, size: 18),
+            ) 
+          ),
+          Positioned(
+            right: 0,
+            child: Container(
+              margin: const EdgeInsets.only(right: 20),
+              child: const Icon(Icons.arrow_forward_ios, size: 18),
+            ) 
           )
         ],
       )
